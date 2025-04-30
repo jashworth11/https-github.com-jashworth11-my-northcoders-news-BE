b54ae7bab74db15155c8df912280dd2993126f7a
Joe Ashworth <joeashworth3427@gmail.com>
2025-04-30 15:58:25 +0100

    pushing anything that needs pushing

--- db/connection.js
+++ db/connection.js
@@ -1,25 +1,25 @@
 const { Pool } = require("pg");
-
 const ENV = process.env.NODE_ENV || "development";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
-
-const db = new Pool();
-
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
+if (ENV === "production") {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL not set in production");
+  }
 } else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
+  if (!process.env.PGDATABASE) {
+    throw new Error("No PGDATABASE configured for development");
+  }
 }
+
 const config =
   ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        ssl: { rejectUnauthorized: false },
         max: 2,
       }
     : {};
 
+const db = new Pool(config);
 module.exports = db;
