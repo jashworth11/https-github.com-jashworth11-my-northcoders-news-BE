9c3a6f494a9c04e27e52f891bebc260068b2d019
Joe Ashworth <joeashworth3427@gmail.com>
2025-04-30 15:28:29 +0100

    i think i messed up here, task 12 and 13 in this one..

--- app/models/model.js
+++ app/models/model.js
@@ -8,8 +8,22 @@ exports.getTopics = () => {
 
 exports.selectArticleById = (article_id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
+    .query(
+      `
+      SELECT 
+        articles.*,
+        COUNT(comments.comment_id)::INT AS comment_count
+      FROM articles
+      LEFT JOIN comments ON articles.article_id = comments.article_id
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id
+    `,
+      [article_id]
+    )
     .then((result) => {
+      if (!result.rows[0]) {
+        return Promise.reject({ status: 404, msg: "not found!" });
+      }
       return result.rows[0];
     });
 };
--- listen.js
+++ listen.js
@@ -1,10 +1,6 @@
 const app = require("./app.js");
 
-app.listen(9090, (err) => {
-  if (err) {
-    console.log(err);
-  } else console.log("Server is listening on port 9090...");
-});
+const app = require("./app");
+const { PORT = 9090 } = process.env;
 
-
-
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
