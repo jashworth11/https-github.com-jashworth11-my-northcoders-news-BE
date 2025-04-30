5828a53a2725f6fe9e07a34cf3e06ab0744e1e2c
Joe Ashworth <joeashworth3427@gmail.com>
2025-04-30 16:16:11 +0100

    trying to fix render error

--- db/connection.js
+++ db/connection.js
@@ -1,25 +1,23 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
-require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+const config = {};
 if (ENV === "production") {
   if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL not set in production");
   }
+  config.connectionString = process.env.DATABASE_URL;
+  config.ssl = { rejectUnauthorized: false };
+  config.max = 2;
 } else {
   if (!process.env.PGDATABASE) {
     throw new Error("No PGDATABASE configured for development");
   }
 }
 
-const config =
-  ENV === "production"
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false },
-        max: 2,
-      }
-    : {};
-
 const db = new Pool(config);
+console.log(`Connected to ${ENV} database`);
 module.exports = db;
